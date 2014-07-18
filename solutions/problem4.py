fb1d12cef922f776e7da33a0edfd89ca54f4398b
slowkid <slow.kid@hotmail.com>
2014-07-17 20:45:03 -0500

    lets see

--- solutions/problem4.py
+++ solutions/problem4.py
@@ -9,3 +9,25 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
 print "problem4"
+
+for a in range(1000):
+    for b in range(1000):
+        c = a * b
+
+print "done"
+
+def is_palindrome(candidate):
+    """
+    Determine if candidate is palindromic. candidate must be 'int' or 'str'
+    """
+    if type(candidate) in (int, long):
+        candidate = str(candidate)
+    if type(candidate) != str:
+        raise TypeError("is_palindrome(): candidate is not 'str' or 'int' type. type(candidate) = %s" % type(candidate))
+    candidate = 'c' + candidate
+    
+    print candidate[0], candidate[-1]
+    
+    
+    
+print is_palindrome(314159265358979323846264338327950288419716939937510582)
