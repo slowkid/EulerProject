e51ca0fd10d9e49c7b6ab4681f845ab3a1b40268
slowkid <slow.kid@hotmail.com>
2014-07-17 23:27:49 -0500

    a solution and some bullshit

--- solutions/problem4.py
+++ solutions/problem4.py
@@ -19,15 +19,36 @@ print "done"
 def is_palindrome(candidate):
     """
     Determine if candidate is palindromic. candidate must be 'int' or 'str'
+    
+    Notice the slicing notation in the return line.  Slice notation is
+    [start:end:stride].  Any of these can be left blank and will default
+    as follows: start == blank is beginning of str.  end == blank is end of
+    string.  stride == blank is +1
     """
     if type(candidate) in (int, long):
         candidate = str(candidate)
     if type(candidate) != str:
         raise TypeError("is_palindrome(): candidate is not 'str' or 'int' type. type(candidate) = %s" % type(candidate))
-    candidate = 'c' + candidate
+        
+    #return whether candidate == reverse of itself.  ie is a palindrome
+    return candidate == candidate[::-1]
     
-    print candidate[0], candidate[-1]
-    
-    
-    
-print is_palindrome(314159265358979323846264338327950288419716939937510582)
+def main():
+    largest = 0
+    for a in range(100,1000):
+        for b in range(100,1000):
+            product = a * b
+            if is_palindrome(product):
+                if product > largest:
+                    largest = product
+    print(largest)
+
+if __name__ == "__main__":
+    #main()
+    word = "thunderbox"
+    print word[:]
+    print word[::]
+    print word[::1]
+    print word[::-1]
+    print word[::-2]
+    print word[::-1][::-1]
