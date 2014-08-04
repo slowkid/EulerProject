fb8d93784c8d58a4ed98b6b011b207ef991edb3d
slowkid <slow.kid@hotmail.com>
2014-08-03 20:28:08 -0500

    I only kind of work.  Fuck you.

--- solutions/problem17.py
+++ solutions/problem17.py
@@ -16,38 +16,14 @@ Do not count spaces or hyphens. For example, 342 (three hundred and forty-two)
 contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The 
 use of "and" when writing out numbers is in compliance with British usage.
 """
-#------------------------------------------------------------------------------
-import re
- 
 #------------------------------------------------------------------------------ 
-def split_by_n( seq, n ):
-    """A generator to divide a sequence into chunks of n units."""
-    while seq:
-        yield seq[:n]
-        seq = seq[n:]
-
-print list(split_by_n("123456789",2))
-
-# This one is ok but fails for odd number of digits
-s = '1234567890'
-print map(''.join, zip(*[iter(s)]*2))
-
-
-print re.findall('..?', '1234567890')
-
 def split_triples(number):
     number = str(number)
     while number:
         yield int(number[-3:])
         number = number[:-3]
-        
-print list(split_triples(123456789))
 
-#------------------------------------------------------------------------------
 def convert_digit(digit):
-    """
-    digit should be positive integer from 0 to 9
-    """
     digit_words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
     if digit < 0 or digit > 9:
@@ -56,14 +32,10 @@ def convert_digit(digit):
     return digit_words[digit] 
 
 def convert_double_digits(ddn):
-    """
-    ddn should be a double digit number
-    """
-    #print "convert_double_digits(): ddn =", ddn 
     teen_words = ["ten", "eleven", "twelve", "thirteen", "fourteen", 
                   "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
     tens_words = ["N/A", "N/A", "twenty", "thirty", "forty",
-                  "fifty", "sixty", "seventy", "eigthty", "ninety"]
+                  "fifty", "sixty", "seventy", "eighty", "ninety"]
     
     if ddn < 10:        
         result = convert_digit(ddn)        
@@ -79,20 +51,16 @@ def convert_double_digits(ddn):
     
     return result
     
-def convert_triple_digits(tdn, use_and="and "):
-    """
-    tdn should be a triple digit number
-    """
-    #print "conver_triple_digits(): tdn = ", tdn
+def convert_triple_digits(tdn):
     if tdn < 1000:
         hundreds, ddn = divmod(tdn, 100)
         if hundreds:
             result = convert_digit(hundreds) + " hundred "
+            if ddn:
+                result += "and "
         else:
-            result = ""
-        if use_and:
-            result += use_and
-        if ddn > 0:
+            result = ""        
+        if ddn:
             result += "%s" % convert_double_digits(ddn)
     else:
         raise ValueError("Out of range: tdn = %s" % tdn)
@@ -109,16 +77,14 @@ def convert_number_to_words(number):
     for index, triple in enumerate(triples):
         #result = convert_triple_digits(triple)
         template = "%s %s"
-        if index > 0:
+        if index and triple:
             template += ", "
-            tdn = convert_triple_digits(triple, False)
-        else:
-            tdn = convert_triple_digits(triple)
-        result = template % (tdn, triple_names[index]) + result
+        if triple: 
+            result = template % (convert_triple_digits(triple), triple_names[index]) + result
         if index > 0:
             #result += ", "
             pass
-    return result
+    return number, result
 
 #------------------------------------------------------------------------------ 
 def solve():
@@ -135,12 +101,10 @@ def main():
 #------------------------------------------------------------------------------ 
 if __name__ == "__main__":
     #main()
-    print convert_digit(8)
-    print convert_double_digits(18)
-    print convert_double_digits(88)
-    print convert_triple_digits(818)
-    print convert_triple_digits(888), "\n"
-    #print convert_triple_digits(789)
     print convert_number_to_words(123456789)
-    print convert_number_to_words(11101)
+    #print convert_number_to_words(11101)
+    print convert_number_to_words(1000000001)
+    #print convert_number_to_words(1000001)
+    #print convert_number_to_words(1001)
     print convert_number_to_words(101)
+    print convert_number_to_words(1000)
